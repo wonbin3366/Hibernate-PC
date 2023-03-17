0108fc94ccfc06407f69a63b5ffef653f629ac49
wonbin3366 <wonbin3366@naver.com>
2023-03-17 12:09:29 +0900

    Repository 생성완료

--- src/main/java/shop/mtcoding/hiberpc/model/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/hiberpc/model/board/BoardRepository.java
@@ -1,8 +1,43 @@
 package shop.mtcoding.hiberpc.model.board;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import org.springframework.stereotype.Repository;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Repository
 public class BoardRepository {
+    private final EntityManager em;
 
+    public Board findById(int id) {
+        return em.find(Board.class, id);
+    }
+
+    public List<Board> findAll() {
+        return em.createQuery("select b from board b", Board.class).getResultList();
+    }
+
+    // 업데이트는 영속화된거를 집어넣어야함?
+    public Board save(Board board) {
+        if (board.getId() == null) {
+            em.persist(board);
+        } else { // 더티체킹 할거여서 쓸일없음!
+            Board boardPS = em.find(Board.class, board.getId());
+            if (boardPS != null) {
+                em.merge(board);
+            } else {
+                System.out.println("잘못된 머지를 하셨어요!");
+            }
+        }
+
+        return board;
+    }
+
+    public void delete(Board board) {
+        em.remove(board);
+    }
 }
--- src/main/java/shop/mtcoding/hiberpc/model/user/UserRepository.java
+++ src/main/java/shop/mtcoding/hiberpc/model/user/UserRepository.java
@@ -1,8 +1,44 @@
 package shop.mtcoding.hiberpc.model.user;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import org.springframework.stereotype.Repository;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Repository
 public class UserRepository {
+    private final EntityManager em;
+
+    public User findById(int id) {
+        return em.find(User.class, id);
+    }
+
+    public List<User> findAll() {
+        return em.createQuery("select u from User u", User.class).getResultList();
+    }
+
+    // 업데이트는 영속화된거를 집어넣어야함?
+    public User save(User user) {
+        if (user.getId() == null) {
+            em.persist(user);
+        } else { // 더티체킹 할거여서 쓸일없음!
+            User userPS = em.find(User.class, user.getId());
+            if (userPS != null) {
+                em.merge(user);
+            } else {
+                System.out.println("잘못된 머지를 하셨어요!");
+            }
+        }
+
+        return user;
+    }
+
+    public void delete(User user) {
+        em.remove(user);
+    }
 
 }
