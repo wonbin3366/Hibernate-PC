5dd02ee9912d0a284cfe41a6984b61840d068f70
wonbin3366 <wonbin3366@naver.com>
2023-03-17 11:40:56 +0900

    User/Board 테이블 생성 완료

--- src/main/java/shop/mtcoding/hiberpc/model/board/Board.java
+++ src/main/java/shop/mtcoding/hiberpc/model/board/Board.java
@@ -1,5 +1,38 @@
 package shop.mtcoding.hiberpc.model.board;
 
-public class Board {
+import java.sql.Timestamp;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.ForeignKey;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import shop.mtcoding.hiberpc.model.user.User;
+
+@NoArgsConstructor
+@Setter // dto있으면 setter 필요없다
+@Getter
+@Entity
+@Table(name = "board_tb")
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne // 포린키설정
+    private User user;
+
+    private String title;
+    private String content;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
 }
--- src/main/java/shop/mtcoding/hiberpc/model/user/User.java
+++ src/main/java/shop/mtcoding/hiberpc/model/user/User.java
@@ -1,5 +1,45 @@
 package shop.mtcoding.hiberpc.model.user;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter // dto있으면 setter 필요없다
+@Getter
+@Entity
+@Table(name = "user_tb")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    @JsonIgnore
+    private String password;
+    private String email;
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Builder
+    public User(Integer id, String username, String password, String email, Timestamp createdAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
 
 }
